1fe9dd00b3987a354ee5876537866d826c78ae1d
barnaholl <hollbarna@freemail.hu>
2021-09-06 21:10:48 +0200

    project init

---
+++ src/test/java/com/theyellowpug/projectArt/ProjectArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theyellowpug.projectArt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

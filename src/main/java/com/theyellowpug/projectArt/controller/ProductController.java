c538b072789f159ce88f9b6300ab0d90ef6fc301
barnaholl <hollbarna@freemail.hu>
2021-09-15 15:20:44 +0200

    add ProductController

---
+++ src/main/java/com/theyellowpug/projectArt/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.theyellowpug.projectArt.controller;
+
+import com.theyellowpug.projectArt.entity.Product;
+import com.theyellowpug.projectArt.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@RequestParam("id") Long id){
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+}
--- src/main/java/com/theyellowpug/projectArt/service/ProductService.java
+++ src/main/java/com/theyellowpug/projectArt/service/ProductService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ProductServices {
+public class ProductService {
     private final ProductRepository productRepository;
 
     public List<Product> getAllProducts() {

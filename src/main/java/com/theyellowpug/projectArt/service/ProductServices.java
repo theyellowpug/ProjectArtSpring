64f037241922df932d7f6e156ea056571e499bfc
barnaholl <hollbarna@freemail.hu>
2021-09-15 15:12:40 +0200

    add ProductServices

---
+++ src/main/java/com/theyellowpug/projectArt/service/ProductServices.java
@@ -0,0 +1,23 @@
+package com.theyellowpug.projectArt.service;
+
+import com.theyellowpug.projectArt.entity.Product;
+import com.theyellowpug.projectArt.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServices {
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+}

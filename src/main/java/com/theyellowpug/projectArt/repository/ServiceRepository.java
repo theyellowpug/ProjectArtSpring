1524bc7ec5255e8fad38704155c4a24754091266
barnaholl <hollbarna@freemail.hu>
2021-10-17 22:18:12 +0200

    remove service entity

--- src/main/java/com/theyellowpug/projectArt/repository/ServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.theyellowpug.projectArt.repository;
-
-import com.theyellowpug.projectArt.entity.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<Service, Long> {
-}
